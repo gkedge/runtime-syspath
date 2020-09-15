7a082a4b298fce9ef61f2430f74119b56e78c3b5
gkedge <gregwork@kedges.com>
2020-09-15 00:30:55 -0400

    Testing and implementing appending syspath_sleuth.py to system-site customize

--- src/syspath_sleuth/__init__.py
+++ src/syspath_sleuth/__init__.py
@@ -1,12 +1,13 @@
 import argparse
+import difflib
+import inspect
 import logging
-
 from pathlib import Path
-from pprint import pprint
+import shutil
 import site
-from typing import Optional, Sequence
+from typing import Optional, Sequence, List, Iterator
 
-from .syspath_sleuth import SysPathSleuth
+from . import syspath_sleuth
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -16,13 +17,13 @@ class InstallError(RuntimeError):
 
 
 def parse_syspath_sleuth_args(
-    args: Optional[Sequence[str]] = None, parser: argparse.ArgumentParser = None
+        args: Optional[Sequence[str]] = None, parser: argparse.ArgumentParser = None
 ):
     """Parse command line arguments, return argparse namespace."""
     if not parser:
         parser = argparse.ArgumentParser(
             description="(Un)Install SysPathSleuth into user-site or system-site to track sys.path "
-            "access in real-time."
+                        "access in real-time."
         )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
@@ -60,22 +61,71 @@ def append_sleuth_to_usercustomize(user_customize_path: Path):
 
 
 def append_sleuth_to_customize(system_customize_path):
-    sleuth_message = f"Appending SysPathSleuth to system site customize: {system_customize_path}"
-    logger.info(sleuth_message)
+    logger.info("Appending SysPathSleuth to system site customize: %s", system_customize_path)
+    lines: List[str]
+    lines, _ = inspect.getsourcelines(syspath_sleuth)
+
+    with system_customize_path.open('r+') as system_customize_path_f:
+        system_customize_path_f.writelines(lines)
 
 
-def create_site_customize(system_customize_path: Path) -> bool:
-    return False
+def create_site_customize(system_customize_path: Path):
+    logger.info(f"Creating system site {system_customize_path.name}")
+    system_customize_path.touch()
+
+
+def copy_site_customize(system_customize_path: Path):
+    shutil.copy(system_customize_path, system_customize_path.name + '.pre_sleuth')
+
+
+def create_reverse_sleuth_patch(system_customize_path):
+    pre_sleuth_site_customize_path = Path(system_customize_path.stem + '.pre_sleuth')
+    if system_customize_path.exists and pre_sleuth_site_customize_path.exists:
+        system_customize_lines: List[str]
+        with system_customize_path.open() as system_customize_f:
+            system_customize_lines = system_customize_f.readlines()
+
+        pre_sleuth_site_customize_lines: List[str]
+        with pre_sleuth_site_customize_path.open() as pre_sleuth_site_customize_f:
+            pre_sleuth_site_customize_lines = pre_sleuth_site_customize_f.readlines()
+
+        unified_diff_lines: Iterator[str] = \
+            difflib.unified_diff(system_customize_lines, pre_sleuth_site_customize_lines)
+        site_customize_reverse_patch: Path = \
+            system_customize_path.parent / system_customize_path.stem + '_reverse_sleuth.patch'
+
+        with site_customize_reverse_patch.open() as site_customize_reverse_patch_f:
+            site_customize_reverse_patch_f.writelines(unified_diff_lines)
+
+        pre_sleuth_site_customize_path.unlink()
+
+
+def reverse_existing_sleuth(system_customize_path):
+    reverse_patch_path = system_customize_path.parent / system_customize_path.stem + \
+                         '_reverse_sleuth.patch'
+    if reverse_patch_path.exists():
+        pass  # replace with application of reverse patch on system_customize_path
+        reverse_patch_path.unlink()
 
 
 def inject_sleuth_into_system_site():
-    system_customize_path = Path(site.getsitepackages()[0]) / "sitecustomize.py"
-    if system_customize_path.exists():
-        append_sleuth_to_customize(system_customize_path)
-    elif create_site_customize(system_customize_path):
-        pass
+    system_customize_path: Path = get_system_customize_path()
+
+    if system_customize_path and not system_customize_path.exists():
+        create_site_customize(system_customize_path)
     else:
-        raise InstallError("Unable to install SysPathSleuth to system site.")
+        reverse_existing_sleuth(system_customize_path)
+    copy_site_customize(system_customize_path)
+    append_sleuth_to_customize(system_customize_path)
+    create_reverse_sleuth_patch(system_customize_path)
+
+
+def get_system_customize_path() -> Path:
+    system_site: str
+    for system_site in site.getsitepackages():
+        if 'site-packages' in system_site:
+            return Path(system_site) / "sitecustomize.py"
+    raise InstallError("No system site found!")
 
 
 def remove_sleuth_into_user_site():
@@ -90,13 +140,21 @@ def main(args: Optional[Sequence[str]] = None):
     args_namespace: argparse.Namespace = parse_syspath_sleuth_args(args)
 
     if args_namespace.install:
-        if site.ENABLE_USER_SITE and site.check_enableusersite():
-            inject_sleuth_into_user_site()
-        else:
-            inject_sleuth_into_system_site()
+        inject_sleuth()
     elif args_namespace.uninstall:
-        if site.ENABLE_USER_SITE and site.check_enableusersite():
-            remove_sleuth_into_user_site()
-        remove_sleuth_into_system_site()
+        uninstall_sleuth()
     else:
         raise InstallError("Not install or uninstall? Confused")
+
+
+def uninstall_sleuth():
+    if site.ENABLE_USER_SITE and site.check_enableusersite():
+        remove_sleuth_into_user_site()
+    remove_sleuth_into_system_site()
+
+
+def inject_sleuth():
+    if site.ENABLE_USER_SITE and site.check_enableusersite():
+        inject_sleuth_into_user_site()
+    else:
+        inject_sleuth_into_system_site()
--- tests/syspath_sleuth_tests/test___init__.py
+++ tests/syspath_sleuth_tests/test___init__.py
@@ -1,5 +1,8 @@
 import argparse
+import inspect
 import logging
+from pathlib import Path
+from typing import List, Optional
 
 from _pytest.capture import CaptureResult
 import pytest
@@ -44,11 +47,11 @@ def test_parse_args_help(capsys):
         result: CaptureResult = capsys.readouterr()
         assert "usage:" in result.out and "(-i | -u)" in result.out
         assert (
-            "-i, --install    Install SysPathSleuth to user-site if available, else" in result.out
+                "-i, --install    Install SysPathSleuth to user-site if available, else" in result.out
         )
         assert (
-            "-u, --uninstall  Uninstall SysPathSleuth from both user-site and/or system-"
-            in result.out
+                "-u, --uninstall  Uninstall SysPathSleuth from both user-site and/or system-"
+                in result.out
         )
 
 
@@ -70,7 +73,7 @@ def test_parse_args_bad(capsys):
         result: CaptureResult = capsys.readouterr()
         assert "usage:" in result.err and "(-i | -u)" in result.err
         assert (
-            "error: argument -u/--uninstall: not allowed with argument -i/--install" in result.err
+                "error: argument -u/--uninstall: not allowed with argument -i/--install" in result.err
         )
 
 
@@ -78,3 +81,59 @@ def test_main(capsys):
     syspath_sleuth.main(["-u"])
     results: CaptureResult = capsys.readouterr()
     # assert results.out == "{'install': False, 'uninstall': True}\n"
+
+
+def test_append_sleuth_to_customize(request, caplog):
+    caplog.set_level(logging.INFO)
+    site_customize_path = Path('yow')
+
+    def fin():
+        if site_customize_path.exists:
+            site_customize_path.unlink()
+    request.addfinalizer(finalizer=fin)
+
+    syspath_sleuth.create_site_customize(site_customize_path)
+    syspath_sleuth.append_sleuth_to_customize(site_customize_path)
+    record: logging.LogRecord
+    for record in caplog.get_records('call'):
+        if 'Appending SysPathSleuth to system site customize: yow' in record.message:
+            assert record.levelname == 'INFO'
+            break
+    else:
+        pytest.fail("Did not find expected log record.")
+
+    assert site_customize_path.exists()
+    src_lines: List[str]
+    src_lines, _ = inspect.getsourcelines(syspath_sleuth.syspath_sleuth)
+
+    with site_customize_path.open() as site_customize_path_f:
+        site_customize_lines: List[str] = site_customize_path_f.readlines()
+
+    assert src_lines == site_customize_lines
+
+
+def test_get_system_customize_path():
+    system_customize_path: Path = syspath_sleuth.get_system_customize_path()
+    assert system_customize_path and system_customize_path.name == "sitecustomize.py" and \
+           system_customize_path.parent.stem == "site-packages" and \
+           "python" in system_customize_path.parent.parent.stem
+
+
+def test_create_site_customize(request, caplog):
+    test_path = Path("yow.yowsa")
+
+    def fin():
+        if test_path.exists():
+            test_path.unlink()
+
+    request.addfinalizer(finalizer=fin)
+
+    syspath_sleuth.create_site_customize(Path("yow.yowsa"))
+
+    record: logging.LogRecord
+    for record in caplog.get_records('call'):
+        if 'Creating system site: yow.yowsa' in record.message:
+            assert record.levelname == 'INFO'
+            break
+
+    assert test_path.exists()
